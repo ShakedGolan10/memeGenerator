f7398fddb56aa139f21ca25e987aa72f6b421b9d
Shaked Golan <shaked518@gmail.com>
2022-12-17 23:12:33 +0200

    css done! - need to improve @media

--- js/meme.service.js
+++ js/meme.service.js
@@ -7,24 +7,24 @@ let gKeys = ['funny', `dogs`, `cat`, `baby`,
 ]
 let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 let gImgs = [
-    { id: 1, url: '/meme-imgs(square)/1.jpg', keywords: ['funny', `politician`] },
-    { id: 2, url: '/meme-imgs(square)/2.jpg', keywords: ['dogs'] },
-    { id: 3, url: '/meme-imgs(square)/3.jpg', keywords: [`baby`, 'dogs'] },
-    { id: 4, url: '/meme-imgs(square)/4.jpg', keywords: ['cat'] },
-    { id: 5, url: '/meme-imgs(square)/5.jpg', keywords: ['baby', 'funny'] },
-    { id: 6, url: '/meme-imgs(square)/6.jpg', keywords: ['person'] },
-    { id: 7, url: '/meme-imgs(square)/7.jpg', keywords: ['baby', 'suprise'] },
-    { id: 8, url: '/meme-imgs(square)/8.jpg', keywords: ['curious', 'person'] },
-    { id: 9, url: '/meme-imgs(square)/9.jpg', keywords: ['funny', 'baby'] },
-    { id: 10, url: '/meme-imgs(square)/10.jpg', keywords: ['funny', 'politician'] },
-    { id: 11, url: '/meme-imgs(square)/11.jpg', keywords: ['fight'] },
-    { id: 12, url: '/meme-imgs(square)/12.jpg', keywords: ['person', `celeb`] },
-    { id: 13, url: '/meme-imgs(square)/13.jpg', keywords: ['toast', `celeb`] },
-    { id: 14, url: '/meme-imgs(square)/14.jpg', keywords: ['matrix'] },
-    { id: 15, url: '/meme-imgs(square)/15.jpg', keywords: ['celeb'] },
-    { id: 16, url: '/meme-imgs(square)/16.jpg', keywords: ['suprise'] },
-    { id: 17, url: '/meme-imgs(square)/17.jpg', keywords: ['politician'] },
-    { id: 18, url: '/meme-imgs(square)/18.jpg', keywords: ['toy-story'] }
+    { id: 1, url: 'meme-imgs(square)/1.jpg', keywords: ['funny', `politician`] },
+    { id: 2, url: 'meme-imgs(square)/2.jpg', keywords: ['dogs'] },
+    { id: 3, url: 'meme-imgs(square)/3.jpg', keywords: [`baby`, 'dogs'] },
+    { id: 4, url: 'meme-imgs(square)/4.jpg', keywords: ['cat'] },
+    { id: 5, url: 'meme-imgs(square)/5.jpg', keywords: ['baby', 'funny'] },
+    { id: 6, url: 'meme-imgs(square)/6.jpg', keywords: ['person'] },
+    { id: 7, url: 'meme-imgs(square)/7.jpg', keywords: ['baby', 'suprise'] },
+    { id: 8, url: 'meme-imgs(square)/8.jpg', keywords: ['curious', 'person'] },
+    { id: 9, url: 'meme-imgs(square)/9.jpg', keywords: ['funny', 'baby'] },
+    { id: 10, url: 'meme-imgs(square)/10.jpg', keywords: ['funny', 'politician'] },
+    { id: 11, url: 'meme-imgs(square)/11.jpg', keywords: ['fight'] },
+    { id: 12, url: 'meme-imgs(square)/12.jpg', keywords: ['person', `celeb`] },
+    { id: 13, url: 'meme-imgs(square)/13.jpg', keywords: ['toast', `celeb`] },
+    { id: 14, url: 'meme-imgs(square)/14.jpg', keywords: ['matrix'] },
+    { id: 15, url: 'meme-imgs(square)/15.jpg', keywords: ['celeb'] },
+    { id: 16, url: 'meme-imgs(square)/16.jpg', keywords: ['suprise'] },
+    { id: 17, url: 'meme-imgs(square)/17.jpg', keywords: ['politician'] },
+    { id: 18, url: 'meme-imgs(square)/18.jpg', keywords: ['toy-story'] }
 ];
 
 
